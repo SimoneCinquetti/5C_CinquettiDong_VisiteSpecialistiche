c236d76a6b2fd40114409f3f81f3a83379a1b66b
Simone Cinquetti <cinquettisimone@itis-molinari.eu>
2024-10-31 12:18:11 +0100

    aggiunta e importata nello script libreria per la gestione tramite gestore della cache remota

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import { initTable } from './components/table.js';
 import { createListOfButtons } from './components/listOfButtons.js';
 import { createForm } from './components/form.js';
 import { getMondayOfDate } from './utils.js';
+import { gestorePrenotazioniCache } from './librerie/prenotazioneCacheRemota.js';
 
 const table = initTable(document.getElementById("tabella"));
 table.build("2024-10-28", "");
